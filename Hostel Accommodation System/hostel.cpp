a61eba22dadc4e86225fc7a5cb62c0d046629c44
aksh31at <asharma21_be22@thapar.edu>
2026-03-14 12:18:05 +0530

    second project added

--- Hostel Accommodation System/hostel.cpp
+++ Hostel Accommodation System/hostel.cpp
@@ -25,6 +25,34 @@ class Hostel{
     int getBed(){
         return Bed;
     }
+
+    void reserve(){
+        ifstream in("C:/Users/as625/OneDrive/Desktop/C++Projects/Hostel Accommodation System/hostel.txt");
+        ofstream out("C:/Users/as625/OneDrive/Desktop/C++Projects/Hostel Accommodation System/hosteltemp.txt");
+
+        string line;
+
+        while(getline(in, line)){
+            int pos=line.find("Anantam Hall");
+            if(pos!=string::npos){
+                int bed=Bed-1;
+                Bed=bed;
+
+                stringstream ss;
+                ss<<bed;
+                string strBed= ss.str();
+
+                int bedPos=line.find_last_of(':');
+                line.replace(bedPos+1, string::npos, strBed);
+            }
+            out<<line<<endl;
+        }
+        out.close();
+        in.close();
+        remove("C:/Users/as625/OneDrive/Desktop/C++Projects/Hostel Accommodation System/hostel.txt");
+        rename("C:/Users/as625/OneDrive/Desktop/C++Projects/Hostel Accommodation System/hosteltemp.txt","C:/Users/as625/OneDrive/Desktop/C++Projects/Hostel Accommodation System/hostel.txt");
+        cout<<"\tBed Reserved Succesfully";
+    }
 };
 
 
@@ -58,11 +86,48 @@ class Student{
 
 int main(){
     Hostel h("Anantam Hall", 56000, 900);
+    ofstream out("C:/Users/as625/OneDrive/Desktop/C++Projects/Hostel Accommodation System/hostel.txt");
+    out<<"\t"<<h.getName()<<" : "<<h.getRent()<<" : "<<h.getBed()<<endl<<endl;
+    cout<<"Hostel Data Saved."<<endl;
+    out.close();
+
     Student s;
 
     bool exit= 0;
     while(!exit){
         system("cls");
-        cout<<"\tWelcome To Hostel Accommodation "
+        int val;
+        cout<<"\tWelcome To Hostel Accommodation System"<<endl;
+        cout<<"\t**************************************"<<endl;
+        cout<<"\t1.Reserve a Bed"<<endl;
+        cout<<"\t2.Exit"<<endl;
+        cout<<"\tEnter Choice: "<<endl;
+        cin>>val;
+
+        if(val==1){
+            system("cls");
+            string name, rollno, address;
+            cout<<"\tEnter Name of Student: "<<endl;
+            cin>>name;
+            s.setName(name);
+            cout<<"\tEnter Roll Number of Student: "<<endl;
+            cin>>rollno;
+            s.setRollNo(rollno);
+            cout<<"\tEnter Address of Student: "<<endl;
+            cin>>address;
+            s.setAddress(address);
+            if(h.getBed()>0){
+                h.reserve();
+            }
+            else if(h.getBed()==0){
+                cout<<"\tSorry! Bed Not Available"<<endl;
+            }
+
+            ofstream outFile("C:/Users/as625/OneDrive/Desktop/C++Projects/Hostel Accommodation System/students.txt", ios::app);
+            outFile<<"\t"<<s.getName()<<" : "<<s.getRollNo()<<" : "<<s.getAddress()<<endl<<endl;
+            cout<<"Student Data Saved."<<endl; 
+            outFile.close();          
+
+        }
     }
 }
